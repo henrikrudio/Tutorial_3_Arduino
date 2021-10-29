644dcee6e6c1943dfbb86de3d3baa95edca3d220
Henrik Freitas <henrik.tozato@gmail.com>
2021-10-29 07:35:51 -0300

    Implementando funções

--- arduino_CODmorse_v1.0/arduino_CODmorse_v1.0.ino
+++ arduino_CODmorse_v1.0/arduino_CODmorse_v1.0.ino
@@ -1,48 +1,39 @@
-int LED = LED_BUILTIN;
-
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
+  morse_multiple_dot(3);
 
-  digitalWrite(LED, HIGH);
-  delay(750);
-  digitalWrite(LED, LOW);
-  delay(250);
-  digitalWrite(LED, HIGH);
-  delay(750);
-  digitalWrite(LED, LOW);
-  delay(250);
-  digitalWrite(LED, HIGH);
-  delay(750);
-  digitalWrite(LED, LOW);
-  delay(250);
+  morse_multiple_dash(3);
 
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
+  morse_multiple_dot(3);
 
   delay(2000);
+}
+
+void dot(){
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(250);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(250);
+}
+
+void dash(){
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(750);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(250);
+}
+
+void morse_multiple_dot(int n){
+  for(int i=0; i<n ; i++){
+    dot();
+  }
+}
+
+void morse_multiple_dash(int n){
+  for(int i=0; i<n ; i++){
+    dash();
+  }
 }
